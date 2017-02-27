a1ef75e6ad78639834450bfa43caf41580d5b9af
Marlena Bowen <marlena@joinroot.com>
2017-02-27 16:03:03 -0500

    Completes first draft of score_for_frames

--- bowling.rb
+++ bowling.rb
@@ -1,11 +1,66 @@
 class Game
-  def roll(pins)
+  def initialize
+    @frame_rolls = []
+    @frame_score = []
+    @frames = []
   end
 
-  def score_for_frame
+  def roll(pins)
+    @frame_rolls.push(pins)
+  end
+
+  def score_for_frame(frame)
+    if frame < 10
+      if @frame_rolls[0] == 10 && !@frame_rolls[1]
+        frame_score = 'X'
+        @frame_score.push(frame_score)
+        result = @frame_score[frame-1]
+      elsif @frame_rolls[0] + @frame_rolls[1] == 10 && !@frame_rolls[2] && @frame_score[frame-1] != 'X'
+        frame_score = '/'
+        @frame_score.push(frame_score)
+        result = @frame_score[frame-1]
+      elsif @frame_rolls[0] + @frame_rolls[1] == 10 && !@frame_rolls[2] && @frame_score[frame-1] == 'X'
+        previous_frame_score = 10 + @frame_rolls[0] + @frame_rolls[1]
+        @frame_score[frame-1] = previous_frame_score
+        current_frame_score = '/'
+        @frame_score.push(current_frame_score)
+        result = @frame_score[frame-1]
+      elsif @frame_score[frame-1] == 'X' && @frame_rolls[0] && @frame_rolls[1]
+        previous_frame_score = 10 + @frame_rolls[0] + @frame_rolls[1]
+        @frame_score[frame-1] = previous_frame_score
+        current_frame_score = @frame_rolls[0] + @frame_rolls[1]
+        @frame_score.push(current_frame_score)
+        result = @frame_score[frame-1]
+      elsif @frame_score[frame-1] == '/' && @frame_rolls[0]
+        previous_frame_score = 10 + @frame_rolls[0]
+        @frame_score[frame-1] = previous_frame_score
+        current_frame_score = @frame_rolls[0] + @frame_rolls[1]
+        @frame_score.push(current_frame_score)
+        result = @frame_score[frame-1]
+      else
+        frame_score = @frame_rolls[0] + @frame_rolls[1]
+        @frame_score.push(frame_score)
+        result = @frame_score[frame-1]
+      end
+    else
+      if @frame_rolls[0] == 10 || @frame_rolls[0] + @frame_rolls[1] == 10
+        frame_score = @frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2]
+        @frame_score.push(frame_score)
+        result = @frame_score[0]
+      else
+        frame_score = @frame_rolls[0] + @frame_rolls[1]
+        @frame_score.push(frame_score)
+        result = @frame_score.last
+      end
+    end
+    @frame_rolls.clear
+    result
   end
 
   def total_score
+    ## if last score is '/' or 'X', return sum of all scores before that
+    ## else return sum of all scores
+    @frame_score
   end
 end
 
@@ -26,6 +81,3 @@ end
   # record a score of 10 + the score from the first roll of the next frame to this frame
 
 # if a strike or spare are rolled in the final frame, allow one additional roll
-
-
-### keep track of each frame and number of frames passed
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -6,127 +6,121 @@ describe Game do
     context "when a strike is rolled" do
       it "records a score of 10 + the next frame's score" do
         game = Game.new
-
         game.roll(10)
+        expect(game.score_for_frame(1)).to eq('X')
 
         game.roll(7)
-        game.roll(1)
-
-        expect(game.score_for_frame).to eq(26)
+        game.roll(3)
+        expect(game.score_for_frame(1)).to eq(20)
       end
     end
 
     context "when a spare is rolled" do
       it "records a score of 10 + the next roll's score'" do
         game = Game.new
-
-        game.roll(7)
-        game.roll(3)
+        game.roll(0)
+        game.roll(10)
+        expect(game.score_for_frame(1)).to eq('/')
 
         game.roll(2)
-
-        expect(game.score_for_frame).to eq(12)
+        game.roll(4)
+        expect(game.score_for_frame(1)).to eq(12)
       end
     end
 
     context "more than 0 but less than ten pins were hit" do
       it "returns the total score" do
         game = Game.new
-
         game.roll(2)
         game.roll(6)
-
-        expect(game.score_for_frame).to eq(8)
+        expect(game.score_for_frame(1)).to eq(8)
       end
     end
 
     context "no pins were hit" do
       it "returns the total score of 0" do
         game = Game.new
-
         game.roll(0)
         game.roll(0)
-
-        expect(game.score_for_frame).to eq(0)
+        expect(game.score_for_frame(1)).to eq(0)
       end
     end
 
     context "in the final frame" do
       it "adds a third roll if a strike was rolled and scores appropriately" do
         game = Game.new
-
         game.roll(10)
-
         game.roll(3)
         game.roll(2)
-
-        expect(game.score_for_frame).to eq(15)
+        expect(game.score_for_frame(10)).to eq(15)
       end
 
       it "adds a third roll if a spare was rolled and scores appropriately" do
         game = Game.new
-
         game.roll(5)
         game.roll(5)
-
         game.roll(2)
-
-        expect(game.score_for_frame).to eq(12)
+        expect(game.score_for_frame(10)).to eq(12)
       end
 
       it "has only two rolls if neither a strike nor spare was rolled" do
         game = Game.new
-
         game.roll(4)
         game.roll(4)
-
-        expect(game.score_for_frame).to eq(8)
+        expect(game.score_for_frame(10)).to eq(8)
       end
     end
   end
 
-  describe "entire game" do
+  context "entire game" do
     context "typical game" do
       it "returns the total score" do
         game = Game.new
 
         game.roll(10)
+        expect(game.score_for_frame(1)).to eq('X')
 
-        game.roll(3)
         game.roll(7)
+        game.roll(3)
+        expect(game.score_for_frame(1)).to eq(20)
+        expect(game.total_score).to eq(20)
 
         game.roll(5)
         game.roll(2)
+        expect(game.total_score).to eq(42)
 
         game.roll(10)
+        expect(game.total_score).to eq(42)
 
         game.roll(0)
         game.roll(0)
+        expect(game.total_score).to eq(52)
 
         game.roll(7)
         game.roll(3)
+        expect(game.total_score).to eq(52)
 
         game.roll(1)
         game.roll(0)
+        expect(game.total_score).to eq(64)
 
         game.roll(0)
         game.roll(10)
+        expect(game.total_score).to eq(64)
 
         game.roll(4)
         game.roll(4)
+        expect(game.total_score).to eq(86)
 
         game.roll(1)
-        game.roll(9)
-        game.roll(10)
-
-        expect(game.total_score).to eq(106)
+        game.roll(8)
+        expect(game.total_score).to eq(95)
       end
     end
 
     context "perfect game" do
       it "returns a score of 300" do
         game = Game.new
-
         game.roll(10)
         game.roll(10)
         game.roll(10)
@@ -136,11 +130,9 @@ describe Game do
         game.roll(10)
         game.roll(10)
         game.roll(10)
-
         game.roll(10)
         game.roll(10)
         game.roll(10)
-
         expect(game.total_score).to eq(300)
       end
     end
