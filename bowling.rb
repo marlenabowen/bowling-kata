b49fe64d2db9f9e061a66d0bc4991f90f606491e
Marlena Bowen <marlena@joinroot.com>
2017-02-27 11:03:33 -0500

    Psuedo code

--- bowling.rb
+++ bowling.rb
@@ -1,4 +1,31 @@
-class Bowling
-  def self.()
+class Game
+  def roll(pins)
+  end
+
+  def score_for_frame
+  end
+
+  def total_score
   end
 end
+
+# accept a score for each roll equivalent to the number of pins knocked down, determine if strike
+
+# if a strike is rolled:
+  # do not accept another roll for that frame
+  # record 'X' for this frame until next frame is finished
+  # when the next frame is finished, record a score of 10 + the second frame's score to this frame
+
+# group every two rolls or a strike into a frame
+  # add those two roll scores to get the frame score
+  # determine if spare
+  # limit to 10 frames
+
+# if a spare is rolled:
+  # record '/' for this frame until next frame is finished
+  # record a score of 10 + the score from the first roll of the next frame to this frame
+
+# if a strike or spare are rolled in the final frame, allow one additional roll
+
+
+### keep track of each frame and number of frames passed
