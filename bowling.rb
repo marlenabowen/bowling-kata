747f31c36abb2255e7cf37b367564f3da831abbd
Leeroy Jenkins <jenkins@leeroy.local>
2017-03-01 12:14:46 -0500

    Refactors 4

--- bowling.rb
+++ bowling.rb
@@ -8,147 +8,33 @@
     @frame_rolls.push(pins)
   end
 
-  def frame_score
+  def frame_score(frame)
+    if frame == 10
+      frame = 'final'
+    end
+
     if current_frame_strike?
-      result = handle_strike_in_current_frame('typical frame')
+      result = frame_score_for_strike_or_spare(frame, 'X')
     elsif current_frame_spare?
-      result = handle_spare_in_current_frame
+      result = frame_score_for_strike_or_spare(frame, '/')
      else
-      result = handle_current_frame
+      result = frame_score_without_strike_or_spare
     end
     @frame_rolls.clear
     result
   end
 
-  def final_frame_score
-    if previous_frame_strike?
-      if current_frame_strike?
-        if frame_before_previous_is_strike?
-          handle_three_strikes_in_a_row('final frame')
-        else
-          handle_previous_frame_score(20, @frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2])
-        end
-      elsif current_frame_spare?
-        handle_previous_frame_score(20, @frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2])
-      else
-        handle_previous_frame_score(10 + @frame_rolls[0] + @frame_rolls[1], @frame_rolls[0] + @frame_rolls[1])
-      end
-    elsif previous_frame_spare?
-      if current_frame_strike? || current_frame_spare?
-        handle_previous_frame_score(10 + @frame_rolls[0], @frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2])
-      else
-        handle_previous_frame_score(10 + @frame_rolls[0], @frame_rolls[0] + @frame_rolls[1])
-      end
-    else
-      if current_frame_strike? || current_frame_spare?
-        update_frames_scores(@frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2])
-      else
-        update_frames_scores(@frame_rolls[0] + @frame_rolls[1])
-      end
-    end
-  end
-
   def total_score
-    number_of_frames = @frames_scores.length
     if previous_frame_strike?
-      @frames_scores.pop
-      if @frames_scores[number_of_frames-2] == 'X' # and if the frame before the last frame was a strike
-        result = update_total_score(30)
-      elsif @frames_scores[number_of_frames-2] == '/' # or if the frame before the last frame was a spare
-        result = update_total_score(20)
-      else #  or if the frame before the last frame was neither a strike nor a spare
-        result = @frames_scores.inject(0) { |sum,x| sum + x }
-      end
-      @frames_scores.push('X')
-    elsif @frames_scores.last == '/' # or if the last frame was a spare
-      @frames_scores.pop
-      if @frames_scores[number_of_frames-2] == 'X' # and if the frame before the last frame was a strike
-        result = update_total_score(30)
-      elsif @frames_scores[number_of_frames-2] == '/' # or if the frame before the last frame was a spare
-        result = update_total_score(30)
-      else # or if the frame before the last frame was neither a strike nor a spare
-        result = @frames_scores.inject(0) { |sum,x| sum + x }
-      end
-      @frames_scores.push('/')
-    else # or if the last frame was neither a strike nor a spare
-      result = @frames_scores.inject(0) { |sum,x| sum + x }
+      sum_if_previous_frame_is_strike_or_spare('X')
+    elsif previous_frame_spare?
+      sum_if_previous_frame_is_strike_or_spare('/')
+    else
+      sum
     end
-    result
   end
 
   private
-
-  def handle_strike_in_current_frame(frame)
-    if previous_frame_strike?
-      if frame_before_previous_is_strike?
-        handle_three_strikes_in_a_row(frame)
-      else
-        handle_previous_frame_score(20, 'X')
-      end
-    else
-      update_frames_scores('X')
-    end
-  end
-
-  def handle_spare_in_current_frame
-    if previous_frame_strike?
-      handle_previous_frame_score(20, '/')
-    elsif previous_frame_spare?
-      handle_previous_frame_score(10 + @frame_rolls[0], '/')
-    else
-      update_frames_scores('/')
-    end
-  end
-
-  def handle_current_frame
-    first_roll = @frame_rolls[0]
-    score_for_current_frame = first_roll + @frame_rolls[1]
-    if previous_frame_strike?
-      handle_score_after_strike
-    elsif previous_frame_spare?
-      handle_previous_frame_score(10 + first_roll, score_for_current_frame)
-    else
-      update_frames_scores(score_for_current_frame)
-    end
-  end
-
-  def handle_three_strikes_in_a_row(frame)
-    @frames_scores.pop
-    @frames_scores.pop
-    @frames_scores.push(30)
-    if frame == 'final frame'
-      @frames_scores.push(30)
-      update_frames_scores(@frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2])
-    else
-      @frames_scores.push(20)
-      update_frames_scores('X')
-    end
-  end
-
-  def update_frames_scores(frame_score)
-    @frames_scores.push(frame_score)
-    @frames_scores.last
-  end
-
-  def update_total_score(updated_score)
-    @frames_scores.pop
-    @frames_scores.push(updated_score)
-    @frames_scores.inject(0) { |sum,x| sum + x }
-  end
-
-  def handle_previous_frame_score(previous_frame_score, current_frame_score)
-    @frames_scores.pop
-    @frames_scores.push(previous_frame_score)
-    update_frames_scores(current_frame_score)
-  end
-
-  def handle_score_after_strike
-    score_for_current_frame = @frame_rolls[0] + @frame_rolls[1]
-    @frames_scores.pop
-    @frames_scores.push(10 + score_for_current_frame)
-    @frames_scores.push(score_for_current_frame)
-    @frames_scores.last
-  end
 
   def current_frame_strike?
     @frame_rolls[0] == 10
@@ -167,25 +53,70 @@
   end
 
   def frame_before_previous_is_strike?
-    number_of_frames = @frames_scores.length
-    @frames_scores[number_of_frames-2] == 20
+    @frames_scores[@frames_scores.length-2] == 20
+  end
+
+  def frame_before_previous_is_spare?
+    @frames_scores[@frames_scores.length-1] == '/'
+  end
+
+  def frame_score_for_strike_or_spare(frame, current_frame_score)
+    if frame == 'final'
+      current_frame_score = @frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2]
+    end
+
+    if previous_frame_strike? && frame_before_previous_is_strike?
+      three_strikes_in_a_row(frame)
+    elsif previous_frame_strike?
+      handle_previous_frame_score(20, current_frame_score)
+    elsif previous_frame_spare?
+      handle_previous_frame_score(10 + @frame_rolls[0], current_frame_score)
+    else
+      @frames_scores.push(current_frame_score)
+    end
+    current_frame_score
+  end
+
+  def frame_score_without_strike_or_spare
+    current_frame_score = @frame_rolls[0] + @frame_rolls[1]
+    if previous_frame_strike?
+      handle_previous_frame_score(10 + current_frame_score, current_frame_score)
+    elsif previous_frame_spare?
+      handle_previous_frame_score(10 + @frame_rolls[0], current_frame_score)
+    else
+      @frames_scores.push(current_frame_score)
+    end
+    current_frame_score
+  end
+
+  def sum_if_previous_frame_is_strike_or_spare(current_frame_score)
+    @frames_scores.pop
+    result = sum
+    @frames_scores.push(current_frame_score)
+    result
+  end
+
+  def sum
+    @frames_scores.inject(0) { |sum,x| sum + x }
+  end
+
+  def three_strikes_in_a_row(frame)
+    @frames_scores.pop
+    update_total_score(30)
+    if frame == 'final'
+      @frames_scores.push(30, @frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2])
+    else
+      @frames_scores.push(20, 'X')
+    end
+  end
+
+  def handle_previous_frame_score(previous_frame_score, current_frame_score)
+    update_total_score(previous_frame_score)
+    @frames_scores.push(current_frame_score)
+  end
+
+  def update_total_score(updated_score)
+    @frames_scores.pop
+    @frames_scores.push(updated_score)
   end
 end
-
-# accept a score for each roll equivalent to the number of pins knocked down, determine if strike
-
-# if a strike is rolled:
-  # do not accept another roll for that frame
-  # record 'X' for this frame until next frame is finished
-  # when the next frame is finished, record a score of 10 + the second frame's score to this frame
-
-# group every two rolls or a strike into a frame
-  # add those two roll scores to get the frame score
-  # determine if spare
-  # limit to 10 frames
-
-# if a spare is rolled:
-  # record '/' for this frame until next frame is finished
-  # record a score of 10 + the score from the first roll of the next frame to this frame
-
-# if a strike or spare are rolled in the final frame, allow one additional roll
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -7,11 +7,11 @@ describe Game do
       it "records a score of 10 + the next frame's score" do
         game = Game.new
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(1)).to eq('X')
 
         game.roll(7)
         game.roll(3)
-        expect(game.frame_score).to eq('/')
+        expect(game.frame_score(2)).to eq('/')
       end
     end
 
@@ -20,11 +20,11 @@ describe Game do
         game = Game.new
         game.roll(0)
         game.roll(10)
-        expect(game.frame_score).to eq('/')
+        expect(game.frame_score(1)).to eq('/')
 
         game.roll(2)
         game.roll(4)
-        expect(game.frame_score).to eq(6)
+        expect(game.frame_score(2)).to eq(6)
       end
     end
 
@@ -33,7 +33,7 @@ describe Game do
         game = Game.new
         game.roll(2)
         game.roll(6)
-        expect(game.frame_score).to eq(8)
+        expect(game.frame_score(1)).to eq(8)
       end
     end
 
@@ -42,7 +42,7 @@ describe Game do
         game = Game.new
         game.roll(0)
         game.roll(0)
-        expect(game.frame_score).to eq(0)
+        expect(game.frame_score(1)).to eq(0)
       end
     end
 
@@ -52,7 +52,7 @@ describe Game do
         game.roll(10)
         game.roll(3)
         game.roll(2)
-        expect(game.final_frame_score).to eq(15)
+        expect(game.frame_score(10)).to eq(15)
       end
 
       it "adds a third roll if a spare was rolled and scores appropriately" do
@@ -60,14 +60,14 @@ describe Game do
         game.roll(5)
         game.roll(5)
         game.roll(10)
-        expect(game.final_frame_score).to eq(20)
+        expect(game.frame_score(10)).to eq(20)
       end
 
       it "has only two rolls if neither a strike nor spare was rolled" do
         game = Game.new
         game.roll(4)
         game.roll(4)
-        expect(game.final_frame_score).to eq(8)
+        expect(game.frame_score(10)).to eq(8)
       end
     end
   end
@@ -78,51 +78,51 @@ describe Game do
         game = Game.new
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(1)).to eq('X')
 
         game.roll(7)
         game.roll(3)
-        expect(game.frame_score).to eq('/')
+        expect(game.frame_score(2)).to eq('/')
         expect(game.total_score).to eq(20)
 
         game.roll(5)
         game.roll(2)
-        expect(game.frame_score).to eq(7)
+        expect(game.frame_score(3)).to eq(7)
         expect(game.total_score).to eq(42)
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(4)).to eq('X')
         expect(game.total_score).to eq(42)
 
         game.roll(0)
         game.roll(0)
-        expect(game.frame_score).to eq(0)
+        expect(game.frame_score(5)).to eq(0)
         expect(game.total_score).to eq(52)
 
         game.roll(7)
         game.roll(3)
-        expect(game.frame_score).to eq('/')
+        expect(game.frame_score(6)).to eq('/')
         expect(game.total_score).to eq(52)
 
-        game.roll(1)
         game.roll(0)
-        expect(game.frame_score).to eq(1)
-        expect(game.total_score).to eq(64)
+        game.roll(10)
+        expect(game.frame_score(7)).to eq('/')
+        expect(game.total_score).to eq(62)
 
         game.roll(0)
         game.roll(10)
-        expect(game.frame_score).to eq('/')
-        expect(game.total_score).to eq(64)
+        expect(game.frame_score(8)).to eq('/')
+        expect(game.total_score).to eq(72)
 
         game.roll(10)
         game.roll(0)
-        expect(game.frame_score).to eq('X')
-        expect(game.total_score).to eq(84)
+        expect(game.frame_score(9)).to eq('X')
+        expect(game.total_score).to eq(92)
 
         game.roll(1)
         game.roll(8)
-        expect(game.final_frame_score).to eq(9)
-        expect(game.total_score).to eq(112)
+        expect(game.frame_score(10)).to eq(9)
+        expect(game.total_score).to eq(120)
       end
     end
 
@@ -130,39 +130,39 @@ describe Game do
       it "returns a score of 300" do
         game = Game.new
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(1)).to eq('X')
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(2)).to eq('X')
         expect(game.total_score).to eq(20)
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(3)).to eq('X')
         expect(game.total_score).to eq(50)
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(4)).to eq('X')
         expect(game.total_score).to eq(80)
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(5)).to eq('X')
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(6)).to eq('X')
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(7)).to eq('X')
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(8)).to eq('X')
 
         game.roll(10)
-        expect(game.frame_score).to eq('X')
+        expect(game.frame_score(9)).to eq('X')
 
         game.roll(10)
         game.roll(10)
         game.roll(10)
-        expect(game.final_frame_score).to eq(30)
+        expect(game.frame_score(10)).to eq(30)
         expect(game.total_score).to eq(300)
       end
     end
