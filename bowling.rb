a6c77784dd075ccc09a29a26b524953cb45f1e7a
Marlena Bowen <marlena@joinroot.com>
2017-03-02 17:53:08 -0500

    Refactors 5

--- bowling.rb
+++ bowling.rb
@@ -1,22 +1,27 @@
  class Game
+
+  FINAL = 'final'.freeze
+  STRIKE = 'X'.freeze
+  SPARE = '/'.freeze
+
   def initialize
     @frame_rolls = []
     @frames_scores = []
   end
 
   def roll(pins)
-    @frame_rolls.push(pins)
+    @frame_rolls << pins
   end
 
   def frame_score(frame)
     if frame == 10
-      frame = 'final'
+      frame = FINAL
     end
 
     if current_frame_strike?
-      result = frame_score_for_strike_or_spare(frame, 'X')
+      result = frame_score_for_strike_or_spare(frame, STRIKE)
     elsif current_frame_spare?
-      result = frame_score_for_strike_or_spare(frame, '/')
+      result = frame_score_for_strike_or_spare(frame, SPARE)
      else
       result = frame_score_without_strike_or_spare
     end
@@ -26,9 +31,9 @@
 
   def total_score
     if previous_frame_strike?
-      sum_if_previous_frame_is_strike_or_spare('X')
+      sum_if_previous_frame_is_strike_or_spare(STRIKE)
     elsif previous_frame_spare?
-      sum_if_previous_frame_is_strike_or_spare('/')
+      sum_if_previous_frame_is_strike_or_spare(SPARE)
     else
       sum
     end
@@ -37,32 +42,32 @@
   private
 
   def current_frame_strike?
-    @frame_rolls[0] == 10
+    @frame_rolls.first == 10
   end
 
   def current_frame_spare?
-    @frame_rolls[0] + @frame_rolls[1] == 10
+    @frame_rolls.first + @frame_rolls[1] == 10
   end
 
   def previous_frame_strike?
-    @frames_scores.last == 'X'
+    @frames_scores.last == STRIKE
   end
 
   def previous_frame_spare?
-    @frames_scores.last == '/'
+    @frames_scores.last == SPARE
   end
 
   def frame_before_previous_is_strike?
-    @frames_scores[@frames_scores.length-2] == 20
+    @frames_scores[-2] == 20
   end
 
   def frame_before_previous_is_spare?
-    @frames_scores[@frames_scores.length-1] == '/'
+    @frames_scores[-1] == SPARE
   end
 
   def frame_score_for_strike_or_spare(frame, current_frame_score)
-    if frame == 'final'
-      current_frame_score = @frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2]
+    if frame == FINAL
+      current_frame_score = @frame_rolls.first + @frame_rolls[1] + @frame_rolls[2]
     end
 
     if previous_frame_strike? && frame_before_previous_is_strike?
@@ -70,21 +75,21 @@
     elsif previous_frame_strike?
       handle_previous_frame_score(20, current_frame_score)
     elsif previous_frame_spare?
-      handle_previous_frame_score(10 + @frame_rolls[0], current_frame_score)
+      handle_previous_frame_score(10 + @frame_rolls.first, current_frame_score)
     else
-      @frames_scores.push(current_frame_score)
+      @frames_scores << current_frame_score
     end
     current_frame_score
   end
 
   def frame_score_without_strike_or_spare
-    current_frame_score = @frame_rolls[0] + @frame_rolls[1]
+    current_frame_score = @frame_rolls.first + @frame_rolls[1]
     if previous_frame_strike?
       handle_previous_frame_score(10 + current_frame_score, current_frame_score)
     elsif previous_frame_spare?
-      handle_previous_frame_score(10 + @frame_rolls[0], current_frame_score)
+      handle_previous_frame_score(10 + @frame_rolls.first, current_frame_score)
     else
-      @frames_scores.push(current_frame_score)
+      @frames_scores << current_frame_score
     end
     current_frame_score
   end
@@ -92,31 +97,31 @@
   def sum_if_previous_frame_is_strike_or_spare(current_frame_score)
     @frames_scores.pop
     result = sum
-    @frames_scores.push(current_frame_score)
+    @frames_scores << current_frame_score
     result
   end
 
   def sum
-    @frames_scores.inject(0) { |sum,x| sum + x }
+    @frames_scores.reduce(0, :+)
   end
 
   def three_strikes_in_a_row(frame)
     @frames_scores.pop
     update_total_score(30)
-    if frame == 'final'
-      @frames_scores.push(30, @frame_rolls[0] + @frame_rolls[1] + @frame_rolls[2])
+    if frame == FINAL
+      @frames_scores += [30, (@frame_rolls.first + @frame_rolls[1] + @frame_rolls[2])]
     else
-      @frames_scores.push(20, 'X')
+      @frames_scores += [20, STRIKE]
     end
   end
 
   def handle_previous_frame_score(previous_frame_score, current_frame_score)
     update_total_score(previous_frame_score)
-    @frames_scores.push(current_frame_score)
+    @frames_scores << current_frame_score
   end
 
   def update_total_score(updated_score)
     @frames_scores.pop
-    @frames_scores.push(updated_score)
+    @frames_scores << updated_score
   end
 end
